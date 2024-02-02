53425ce8d1a48ddc693ed929d005419e4161efbe
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 20:07:10 +0300

    feat: Add createUser route & validation middleware

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,18 @@
-import { getAllUsers, getUserById } from '../controllers/userController'
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+} from '../controllers/userController'
 import { Router } from 'express'
+import validateInput from '../middlewares/validationMiddleware'
+import { userValidationRules } from '../middlewares/validationRules'
+
 const router = Router()
 
 router.get('/users', getAllUsers)
 
 router.get('/users/:id', getUserById)
+
+router.post('/users', userValidationRules, validateInput, createUser)
 
 export default router
