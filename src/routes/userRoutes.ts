085190a8098c6d913abfe737c2155f0fecccc855
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 19:21:28 +0300

    feat: Add route to get all users

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,1 +1,7 @@
-// User Routes
+import { getAllUsers } from '../controllers/userController'
+import { Router } from 'express'
+const router = Router()
+
+router.get('/users', getAllUsers)
+
+export default router
