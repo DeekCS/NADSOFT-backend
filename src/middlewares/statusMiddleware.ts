10716600b7c7a973ada7f3df14c3bd54890a2f89
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 19:18:31 +0300

    refactor: change default status type

--- src/middlewares/statusMiddleware.ts
+++ src/middlewares/statusMiddleware.ts
@@ -1,23 +1,21 @@
-import { Request, Response, NextFunction } from "express";
+import { Request, Response, NextFunction } from 'express'
 
 const setDefaultStatus = (req: Request, res: Response, next: NextFunction) => {
-  // Using type casting to inform TypeScript about the existence of defaultStatus
-  (res as any).defaultStatus = 200;
-  next();
-};
+  res.locals.defaultStatus = 200
+  next()
+}
 
 const handleErrors = (
   err: Error,
   req: Request,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
-  const statusCode =
-    (res as any).statusCode !== 200 ? (res as any).statusCode : 500;
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500
   res.status(statusCode).json({
     error: err.message,
-    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
-  });
-};
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  })
+}
 
-export { setDefaultStatus, handleErrors };
+export { setDefaultStatus, handleErrors }
