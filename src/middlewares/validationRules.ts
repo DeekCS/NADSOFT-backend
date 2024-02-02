993de76c9ab2610ae8623d06ecdf3656837debac
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 20:23:02 +0300

    feat: Add validation rules for country field

--- src/middlewares/validationRules.ts
+++ src/middlewares/validationRules.ts
@@ -3,7 +3,13 @@ import { body, ValidationChain } from 'express-validator'
 export const userValidationRules: ValidationChain[] = [
   body('name').notEmpty().withMessage('Name is required'),
   body('email').isEmail().withMessage('Invalid email'),
-  body('country').notEmpty().withMessage('Country is required'),
+  body('country')
+    .notEmpty()
+    .withMessage('Country is required')
+    .isLength({ min: 3 })
+    .withMessage('Country should be at least 3 characters')
+    .isAlpha()
+    .withMessage('Country should contain only alphabets'),
   body('age')
     .notEmpty()
     .withMessage('Age is required')
