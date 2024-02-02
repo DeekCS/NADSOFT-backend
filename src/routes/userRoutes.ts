200b1b24546190318d38452486a02bc0c4de6c6f
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 19:41:40 +0300

    feat: Add getUserById route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,9 @@
-import { getAllUsers } from '../controllers/userController'
+import { getAllUsers, getUserById } from '../controllers/userController'
 import { Router } from 'express'
 const router = Router()
 
 router.get('/users', getAllUsers)
+
+router.get('/users/:id', getUserById)
 
 export default router
