fa85a3414a666d26dea560f631d2c5e96af6c99a
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 19:21:00 +0300

    feat: implement get all users controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,1 +1,13 @@
-// User controllers
+import { Request, Response, NextFunction } from 'express' // Import Request here
+import db from '../db.server'
+
+const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const users = await db.users.findMany()
+    res.json(users)
+  } catch (error) {
+    next(error)
+  }
+}
+
+export { getAllUsers }
