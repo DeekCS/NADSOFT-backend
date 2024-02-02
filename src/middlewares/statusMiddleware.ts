ef60d6c9a97953f8e938c0e3801e44f518190c37
deekCS <31720001079@std.bau.edu.jo>
2024-02-02 17:59:58 +0300

    feat: Add @types/express and set default status middleware

--- src/middlewares/statusMiddleware.ts
+++ src/middlewares/statusMiddleware.ts
@@ -1,1 +1,23 @@
-// statusMiddleware.js
+import { Request, Response, NextFunction } from "express";
+
+const setDefaultStatus = (req: Request, res: Response, next: NextFunction) => {
+  // Using type casting to inform TypeScript about the existence of defaultStatus
+  (res as any).defaultStatus = 200;
+  next();
+};
+
+const handleErrors = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode =
+    (res as any).statusCode !== 200 ? (res as any).statusCode : 500;
+  res.status(statusCode).json({
+    error: err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+};
+
+export { setDefaultStatus, handleErrors };
