bcf9752c92fb13b989df62e2f8b12a98d7d1753d
deekCS <31720001079@std.bau.edu.jo>
2024-02-03 20:05:20 +0300

    feat: Add Swagger documentation for API endpoints

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,25 +1,108 @@
+import { Router } from 'express';
 import {
   getAllUsers,
   getUserById,
   createUser,
   updateUser,
   deleteUser,
-} from '../controllers/userController'
-import authenticate from '../middlewares/authenticate'
-import validateInput from '../middlewares/validationMiddleware'
-import { userValidationRules } from '../middlewares/validationRules'
-import { Router } from 'express'
+} from '../controllers/userController';
+import authenticate from '../middlewares/authenticate';
+import validateInput from '../middlewares/validationMiddleware';
+import { userValidationRules } from '../middlewares/validationRules';
 
-const router = Router()
+const router = Router();
 
-router.get('/users', getAllUsers)
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Get a list of users
+ *     description: Returns a list of users.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ */
+router.get('/users', getAllUsers);
 
-router.get('/users/:id', getUserById)
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Get a user by ID
+ *     description: Returns a user by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user to return.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A user.
+ */
+router.get('/users/:id', getUserById);
 
-router.post('/users', userValidationRules, validateInput, createUser)
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     description: Creates a new user.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       201:
+ *         description: A user.
+ */
+router.post('/users', userValidationRules, validateInput, createUser);
 
-router.put('/users/:id', userValidationRules, validateInput, updateUser)
+/**
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Update a user by ID
+ *     description: Updates a user by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user to update.
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: A user.
+ */
+router.put('/users/:id', userValidationRules, validateInput, updateUser);
 
-router.delete('/users/:id', authenticate, deleteUser)
+/**
+ * @swagger
+ * /users/{id}:
+ *   delete:
+ *     summary: Delete a user by ID
+ *     description: Deletes a user by ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: The ID of the user to delete.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: No content.
+ */
+router.delete('/users/:id', authenticate, deleteUser);
 
-export default router
+export default router;
